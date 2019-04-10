fadd461826387e32929edaf279aab588600f49eb
Picharillo <jose_picharillo@gmail.com>
2019-04-09 21:07:06 -0300

    pesquisa São Carlos e tela em construção

---
+++ js/consulta.js
@@ -0,0 +1,16 @@
+
+function login(){
+	window.location = 'em-construcao.html';
+}
+
+function pesquisa(){
+
+	var cidade = $('input[name="cidade"]').val();
+	console.log(cidade);
+
+	if (cidade == 'São Carlos' || cidade == 'são carlos') {
+		window.location = 'consulta_SaoCarlos.html';
+	} else {
+		window.location = 'consulta_1.html';
+	}
+}
